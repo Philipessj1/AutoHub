469d8f4a3df746ec3d54448ea1eb1e81dd8f5557
Philipe Da Motta Mello <38898462+Philipessj1@users.noreply.github.com>
2026-04-08 16:20:25 -0300

    Gerando um exe em segundo plano que inicializa o programa com o windows

--- agent/agent_server.py
+++ agent/agent_server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from core.logger import setup_logger
 from core.automation_loader import load_automations
 import threading
+import socket
 
 app = Flask(__name__)
 logger = setup_logger("AutoHUB.agent")
@@ -20,6 +21,15 @@ def run_in_background(fname, func):
     thread = threading.Thread(target=wrapper)
     thread.daemon = True
     thread.start()
+
+def is_already_running(port=5000):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(("0.0.0.0", port))
+        s.close()
+        return False
+    except:
+        return True
 
 @app.route("/")
 def home():
@@ -43,5 +53,9 @@ def run_cmd_route(command):
             })
 
 if __name__ == "__main__":
+    if is_already_running(5000):
+        print("Agent já está rodando!")
+        exit()
+
     logger.info("Iniciando Agent...")
     app.run(host="0.0.0.0", port=5000)
--- core/automation_loader.py
+++ core/automation_loader.py
@@ -1,16 +1,19 @@
 import os
+import sys
 import importlib
 
-AUTOMATIONS_PATH = "automations"
+BASE_DIR = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
 
 def load_automations():
     automations = {}
 
-    for file in os.listdir(AUTOMATIONS_PATH):
-        if file.endswith(".py") and not file.startswith("_"):
+    automations_path = os.path.join(BASE_DIR, "automations")
+
+    for file in os.listdir(automations_path):
+        if file.endswith(".py") and file != "__init__.py":
             module_name = file[:-3]
 
-            module = importlib.import_module(f"{AUTOMATIONS_PATH}.{module_name}")
+            module = importlib.import_module(f"automations.{module_name}")
 
             if hasattr(module, "run"):
                 automations[module_name] = module.run
